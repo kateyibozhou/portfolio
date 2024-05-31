e2cff0bb4699e114058e6e307dd7eba2d9b02718
Priyank🌸 <p_chaudhary@apple.com>
2024-05-31 14:06:57 -0700

    layout changes

--- scripts/main.ts
+++ scripts/main.ts
@@ -3,14 +3,22 @@ interface image {
   alt_text: string;
 }
 
+enum ProjectCategory {
+  PRODUCT_OR_UX_DESIGN,
+  SERVICE_DESIGN,
+  BRANDING_OR_PACKAGING,
+  ILLUSTRATIONS
+}
+
 interface CaseStudy {
   title: string;
   local_path: string;
   year: string;
-  tags: string;
+  tags: ProjectCategory[];
   preview_image: image;
   summary: string;
   full_viewport: boolean;
+  is_featured?: boolean;
 }
 
 document.addEventListener('DOMContentLoaded', () => {
@@ -19,36 +27,171 @@ document.addEventListener('DOMContentLoaded', () => {
       title: 'Boosting User Renewal Rate at ExpressVPN',
       local_path: './posts/product-design-ux/evpn-increase-user-renewal-rate.html',
       year: '2023',
-      tags: 'Product Design, UX Design, Product Management',
+      tags: [ProjectCategory.PRODUCT_OR_UX_DESIGN],
       preview_image: {
         path: './images/dash.png',
         alt_text: 'User renewal banner page: before and after.'
       },
       summary: 'Global renewal revenue increased by 83%',
-      full_viewport: true
+      full_viewport: true,
+      is_featured: true
     },
     {
       title: 'Development Bank of Singapore Information Architecture',
       local_path: './posts/product-design-ux/dbs-info-arch.html',
       year: '2022',
-      tags: 'Product Design, UX Design',
+      tags: [ProjectCategory.PRODUCT_OR_UX_DESIGN],
       preview_image: {
         path: './images/dash.png',
         alt_text: 'Full page view of The Development Bank of Singapore (DBS) website.'
       },
       summary: 'Spearheaded the development of a user-centric Information Architecture for Treasury & Markets content, resulting in significant improvements in user engagement, navigation efficiency, and new business opportunities',
-      full_viewport: false
+      full_viewport: false,
+      is_featured: true
     },
     {
       title: 'Phillips B2B GTM Launch design for TH and ANZ markets',
       local_path: './posts/service-design/phillips-b2b-gtm-launch.html',
       year: '2021',
-      tags: 'Campaign Design, Launch Design',
+      tags: [ProjectCategory.SERVICE_DESIGN, ProjectCategory.BRANDING_OR_PACKAGING],
       preview_image: {
         path: './images/dash.png',
         alt_text: 'A sample launch campaign delivery asset.'
       },
       summary: 'Delivered localised digital and physical campaign assets for both markets.',
+      full_viewport: false,
+      is_featured: true
+    },
+    {
+      title: 'ExpressVPN Mobile Conversion Optimisation',
+      local_path: './posts/product-design-ux/evpn-mobile-conversion-optimisation.html',
+      year: '2023',
+      tags: [ProjectCategory.PRODUCT_OR_UX_DESIGN],
+      preview_image: {
+        path: './images/dash.png',
+        alt_text: 'ExpressVPN plan renewal view on mobile'
+      },
+      summary: 'Design shipped globally, resulted in 18% boost in AOV, multi-million revenue boost',
+      full_viewport: false
+    },
+    {
+      title: 'Localisation for Shell APAC',
+      local_path: './posts/product-design-ux/localisation-for-shell-apac-i.html',
+      year: '2021',
+      tags: [ProjectCategory.PRODUCT_OR_UX_DESIGN],
+      preview_image: {
+        path: './images/dash.png',
+        alt_text: 'Different views of Shell App on mobile'
+      },
+      summary: 'Design shipped globally, 23% increase in user adoption rates, 17% uplift in customer lifetime value',
+      full_viewport: false
+    },
+    {
+      title: 'Localisation for Shell Malaysia',
+      local_path: './home/psc/z/yibo-gpt/portfolio/posts/product-design-ux/localisation-for-shell-malaysia.html',
+      year: '2021',
+      tags: [ProjectCategory.PRODUCT_OR_UX_DESIGN],
+      preview_image: {
+        path: './images/dash.png',
+        alt_text: 'Samples views of Malaysia App on mobile'
+      },
+      summary: 'Design shipped across Malaysia',
+      full_viewport: false
+    },
+    {
+      title: 'ExpressVPN B2B Enterprise Dashboard',
+      local_path: './posts/product-design-ux/evpn-enterprise-dashboard.html',
+      year: '2021',
+      tags: [ProjectCategory.PRODUCT_OR_UX_DESIGN],
+      preview_image: {
+        path: './images/dash.png',
+        alt_text: 'Current ExpressVPN dashboard'
+      },
+      summary: 'Design shipped globally, 95% increase in customer satisfaction rate, 10 new multi-million dollar client contracts signed',
+      full_viewport: false
+    },
+    {
+      title: 'B2C launch of Phillips Vacuum Cleaner',
+      local_path: './posts/service-design/phillips-b2c-vaccuum-launch.html',
+      year: '2021',
+      tags: [ProjectCategory.SERVICE_DESIGN, ProjectCategory.BRANDING_OR_PACKAGING],
+      preview_image: {
+        path: './images/dash.png',
+        alt_text: 'Sample assets of the launch campaign'
+      },
+      summary: 'Shipped in 2021',
+      full_viewport: false
+    },
+    {
+      title: 'Xiamen Hospital Communications Design',
+      local_path: './posts/service-design/xiamen-hospital-comms.html',
+      year: '2021',
+      tags: [ProjectCategory.SERVICE_DESIGN],
+      preview_image: {
+        path: './images/dash.png',
+        alt_text: 'Brand communication strategy planning design draft'
+      },
+      summary: 'Design identify I created is adopted by hospital. Identity is resulted in boost in patient experience metrics',
+      full_viewport: false
+    },
+    {
+      title: 'Rejoice branding and packaging for GC and APAC',
+      local_path: './posts/branding/rejoice-branding.html',
+      year: '2021',
+      tags: [ProjectCategory.SERVICE_DESIGN],
+      preview_image: {
+        path: './images/dash.png',
+        alt_text: 'Rejoice shampoo bottle packaging designs which shipped'
+      },
+      summary: 'Design shipped across GC and APAC, resulted in consumer visibility metrics and revenue',
+      full_viewport: false
+    },
+    {
+      title: 'Sephora Campaign Management and Optimisation',
+      local_path: './posts/branding/sephora-campaign.html',
+      year: '2021',
+      tags: [ProjectCategory.BRANDING_OR_PACKAGING],
+      preview_image: {
+        path: './images/dash.png',
+        alt_text: 'Sample campaign assets'
+      },
+      summary: 'Streamlined campaign resulted in boost in ROI, consumer engagement, and reduction in campaign launch delays',
+      full_viewport: false
+    },
+    {
+      title: 'SK-II Festive 2016',
+      local_path: './posts/branding/sk-ii-festive.html',
+      year: '2021',
+      tags: [ProjectCategory.BRANDING_OR_PACKAGING],
+      preview_image: {
+        path: './images/dash.png',
+        alt_text: ''
+      },
+      summary: 'Boosted consumer engagement and revenue, reduction in production costs and time-to-market',
+      full_viewport: false
+    },
+    {
+      title: '',
+      local_path: '',
+      year: '2021',
+      tags: [],
+      preview_image: {
+        path: './images/dash.png',
+        alt_text: ''
+      },
+      summary: '',
+      full_viewport: false
+    },
+    {
+      title: '',
+      local_path: '',
+      year: '2021',
+      tags: [],
+      preview_image: {
+        path: './images/dash.png',
+        alt_text: ''
+      },
+      summary: '',
       full_viewport: false
     },
   ];
@@ -58,10 +201,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
   caseStudies.forEach((study, index) => {
     const clone = document.importNode(template.content, true);
-    // All case studies are wrapped in an article element with class 'case-study-view'
-    // Each case-study-view has either one case study occupying the full viewport or two case studies side by side
     const caseStudyView = clone.querySelector('.case-study-view') as HTMLElement;
-    // Uncomment the following line to show the case study number
     // (clone.querySelector('.case-study-number') as HTMLElement).textContent = `${index + 1}/${caseStudies.length}`;
     (clone.querySelector('.case-study-title') as HTMLElement).textContent = study.title;
     (clone.querySelector('.case-study-year') as HTMLElement).textContent = study.year;
@@ -74,7 +214,6 @@ document.addEventListener('DOMContentLoaded', () => {
     links.forEach(link => {
       link.href = study.local_path;
     });
-
     // for case studies with full viewport, add the class 'full-viewport' to the case study
     if (study.full_viewport) {
       caseStudyView.classList.add('full-viewport');
